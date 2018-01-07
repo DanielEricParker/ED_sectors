cbe605e91efed62b7589aead70ace030ffa02c59
Daniel E. Parker <daniel_parker@berkeley.edu>
2018-01-07 10:50:51 -0800

    Fixed U <--> U^d issue.

--- dynamics.jl
+++ dynamics.jl
@@ -127,138 +127,136 @@ function correlation1pt(
 	return correlation(psi,evolve,t,Op_mat)
 end
 
-# """
-# Function to return a correlation at a certain type.
+"""
+Function to return a correlation at a certain type.
 
-# #Arguments
-# * 'psi :: Array{ComplexF64}': the (normalized) state vector to measure
-# * 'evolve :: Evolver': an evolver for our Hamiltonian
-# * 't :: Float64': the time to measure at
-# * 'Op :: Array{Complex{Float64},2}': the operator to measure
-# """
-# #thought: for a single-site operator, it's probably way 
-# #better to compute the reduced density matrix
+#Arguments
+* 'psi :: Array{ComplexF64}': the (normalized) state vector to measure
+* 'evolve :: Evolver': an evolver for our Hamiltonian
+* 't :: Float64': the time to measure at
+* 'Op :: Array{Complex{Float64},2}': the operator to measure
+"""
+#thought: for a single-site operator, it's probably way 
+#better to compute the reduced density matrix
 
-# function correlation(
-# 	psi :: Array{Complex{Float64}},
-# 	evolve :: Evolver,
-# 	t :: Float64,
-# 	Op :: Matrix
-# 	)
-# 	len = length(psi)
+function correlation(
+	psi :: Array{Complex{Float64}},
+	evolve :: Evolver,
+	t :: Float64,
+	Op :: Matrix
+	)
+	len = length(psi)
 	
 
-# 	expEigenvals1 = [exp(-im * t * ev) for ev in evolve.evs]
-# 	expEigenvals2 = [exp(im * t * ev) for ev in evolve.evs]
+	expEigenvals1 = [exp(-im * t * ev) for ev in evolve.evs]
+	expEigenvals2 = [exp(im * t * ev) for ev in evolve.evs]
 
-# 	D1 = Diagonal(expEigenvals1)
-# 	D2 = Diagonal(expEigenvals2)
+	D1 = Diagonal(expEigenvals1)
+	D2 = Diagonal(expEigenvals2)
 
-# 	#<psi|Op(t)|psi>
-# 	#	= <psi|exp(-itH) * Op * exp(itH)|psi>
-# 	#	= <psi|U^d*exp(-itD)*U*Op*U^d*exp(itD)*U|psi> 
+	#<psi|Op(t)|psi>
+	#	= <psi|exp(-itH) * Op * exp(itH)|psi>
+	#	= <psi|U*exp(-itD)*U^d*Op*U*exp(itD)*U^d|psi> 
 
-# 	#to make this maximally efficient, one should 
-# 	#take advantage of the sparseness of the diagonal matrices
-# 	#but let's not bother for now
-# 	#
-# 	#I'm not sure how to memory-manage this,
-# 	#so for now I'm just over-writing v many times
+	#to make this maximally efficient, one should 
+	#take advantage of the sparseness of the diagonal matrices
+	#but let's not bother for now
+	#
+	#I'm not sure how to memory-manage this,
+	#so for now I'm just over-writing v many times
 
-# 	v = BLAS.gemv('N',evolve.O,psi) #N for no transpose
+	v = BLAS.gemv('C',evolve.O,psi) #N for no transpose
 	
-# 	v = D2 * v
-# 	#v = BLAS.gemv('N',D2,v) #this is slower
-# 	v = BLAS.gemv('C',evolve.O,v) #C for conjugate transpose
-# 	v = BLAS.gemv('N',Op, v)
-# 	v = BLAS.gemv('N',evolve.O,v) #N for no transpose
-# 	#v = BLAS.gemv('N',D1,v)#this is slower
-# 	v = D1 * v
-# 	v = BLAS.gemv('C',evolve.O,v) #C for conjugate transpose
+	v = D2 * v #this is faster than -> v = BLAS.gemv('N',D2,v) 
+	v = BLAS.gemv('N',evolve.O,v) #C for conjugate transpose
+	v = BLAS.gemv('N',Op, v)
+	v = BLAS.gemv('C',evolve.O,v) #N for no transpose
+	v = D1 * v
+	v = BLAS.gemv('N',evolve.O,v) #C for conjugate transpose
 
-# 	corr = BLAS.dotc(len,psi,1,v,1)
+	corr = BLAS.dotc(len,psi,1,v,1)
 
-# 	return corr
-# end
+	return corr
+end
 
 
-# """
-# Function to return a correlation at a certain type.
-# * 'rho :: Array{Complex{Float64},2}': the (normalized) density matrix
-# * 'evolve :: Evolver': an evolver for our Hamiltonian
-# * 't :: Float64': the time to measure at
-# * 'Op :: Array{Complex{Float64},2}': the operator to measure
+"""
+Function to return a correlation at a certain type.
+* 'rho :: Array{Complex{Float64},2}': the (normalized) density matrix
+* 'evolve :: Evolver': an evolver for our Hamiltonian
+* 't :: Float64': the time to measure at
+* 'Op :: Array{Complex{Float64},2}': the operator to measure
 
-# """
-# function correlation1pt(
-# 	rho :: Array{Complex{Float64},2},
-# 	evolve :: Evolver,
-# 	t :: Float64,
-# 	Op :: Array{Complex{Float64},2}
-# 	)
+"""
+function correlation1pt(
+	rho :: Array{Complex{Float64},2},
+	evolve :: Evolver,
+	t :: Float64,
+	Op :: Array{Complex{Float64},2}
+	)
 
-# 	expEigenvals1 = [exp(-im * t * ev) for ev in evolve.evs]
-# 	expEigenvals2 = [exp(im * t * ev) for ev in evolve.evs]
+	expEigenvals1 = [exp(-im * t * ev) for ev in evolve.evs]
+	expEigenvals2 = [exp(im * t * ev) for ev in evolve.evs]
 
-# 	D1 = Diagonal(expEigenvals1)
-# 	D2 = Diagonal(expEigenvals2)
+	D1 = Diagonal(expEigenvals1)
+	D2 = Diagonal(expEigenvals2)
 
-# 	# Tr[ rho U^d*exp(-itD)*U*Op*U^d*exp(itD)*U ]
-# 	M = D2 * evolve.O #D2 * U
-# 	#M = BLAS.gemm('C','N',D2,evolve.O)
-# 	M = BLAS.gemm('C','N',evolve.O,M) # U^d *
-# 	M = BLAS.gemm('N','N',Op,M)	# Op *
-# 	M = BLAS.gemm('N','N',evolve.O,M) #U *
-# 	M = D1* M #exp(-itD) *
-# 	#M = BLAS.gemm('C','N',D1,M)
-# 	M = BLAS.gemm('C','N',evolve.O,M) #U^d *
-# 	M = BLAS.gemm('N','N',rho,M) #rho *
+	# Tr[ rho U*exp(-itD)*U^d*Op*U*exp(itD)*U^d ]
+	M = D2 * adjoint(evolve.O) #D2 * U^d
+	#M = BLAS.gemm('C','N',D2,evolve.O)
+	M = BLAS.gemm('N','N',evolve.O,M) # U *
+	M = BLAS.gemm('N','N',Op,M)	# Op *
+	M = BLAS.gemm('C','N',evolve.O,M) #U^d *
+	M = D1* M #exp(-itD) *
+	#M = BLAS.gemm('C','N',D1,M)
+	M = BLAS.gemm('N','N',evolve.O,M) #U *
+	M = BLAS.gemm('N','N',rho,M) #rho *
 
-# 	return trace(M)
+	return trace(M)
 
-# end	
-
-
-# """
-# Function to return a correlation at a certain type.
-# * 'rho :: Array{Complex{Float64},2}': the (normalized) density matrix
-# * 'evolve :: Evolver': an evolver for our Hamiltonian
-# * 't :: Float64': the time to measure at
-# * 'Op :: Array{Complex{Float64},2}': the first operator
-# * 'Op :: Array{Complex{Float64},2}': the second operator
-# """
-# #this is essentially the same as before
-# #perhaps I should just combine them
-# function correlation2pt(
-# 	rho :: Array{Complex{Float64},2},
-# 	evolve :: Evolver,
-# 	t :: Float64,
-# 	Op1 :: Array{Complex{Float64},2},
-# 	Op2:: Array{Complex{Float64},2}
-# 	)
+end	
 
 
-# 	expEigenvals1 = [exp(-im * t * ev) for ev in evolve.evs]
-# 	expEigenvals2 = [exp(im * t * ev) for ev in evolve.evs]
+"""
+Function to return a correlation at a certain type.
+* 'rho :: Array{Complex{Float64},2}': the (normalized) density matrix
+* 'evolve :: Evolver': an evolver for our Hamiltonian
+* 't :: Float64': the time to measure at
+* 'Op :: Array{Complex{Float64},2}': the first operator
+* 'Op :: Array{Complex{Float64},2}': the second operator
+"""
+#this is essentially the same as before
+#perhaps I should just combine them
+function correlation2pt(
+	rho :: Array{Complex{Float64},2},
+	evolve :: Evolver,
+	t :: Float64,
+	Op1 :: Array{Complex{Float64},2},
+	Op2:: Array{Complex{Float64},2}
+	)
 
-# 	D1 = Diagonal(expEigenvals1)
-# 	D2 = Diagonal(expEigenvals2)
 
-# 	# Tr[ rho U^d*exp(-itD)*U*Op_1*U^d*exp(itD)*U Op_2]
+	expEigenvals1 = [exp(-im * t * ev) for ev in evolve.evs]
+	expEigenvals2 = [exp(im * t * ev) for ev in evolve.evs]
 
-# 	M = BLAS.gemm('N','N',evolve.O, Op2)
-# 	M = D2 * M #D2 * U
-# 	#M = BLAS.gemm('C','N',D2,evolve.O)
-# 	M = BLAS.gemm('C','N',evolve.O,M) # U^d *
-# 	M = BLAS.gemm('N','N',Op1,M)	# Op *
-# 	M = BLAS.gemm('N','N',evolve.O,M) #U *
-# 	M = D1* M #exp(-itD) *
-# 	#M = BLAS.gemm('C','N',D1,M)
-# 	M = BLAS.gemm('C','N',evolve.O,M) #U^d *
-# 	M = BLAS.gemm('N','N',rho,M) #rho *
+	D1 = Diagonal(expEigenvals1)
+	D2 = Diagonal(expEigenvals2)
 
-# 	return trace(M)
-# end
+	# Tr[ rho U*exp(-itD)*U^d*Op_1*U*exp(itD)*U^d Op_2]
+
+	M = BLAS.gemm('C','N',evolve.O, Op2)
+	M = D2 * M #D2 * U
+	#M = BLAS.gemm('C','N',D2,evolve.O)
+	M = BLAS.gemm('N','N',evolve.O,M) # U *
+	M = BLAS.gemm('N','N',Op1,M)	# Op *
+	M = BLAS.gemm('C','N',evolve.O,M) #U^d *
+	M = D1* M #exp(-itD) *
+	#M = BLAS.gemm('C','N',D1,M)
+	M = BLAS.gemm('N','N',evolve.O,M) #U *
+	M = BLAS.gemm('N','N',rho,M) #rho *
+
+	return trace(M)
+end
 
 """
 Function to compute the thermal density matrix
@@ -278,49 +276,49 @@ function thermal_density_matrix(
 	return M
 end
 
-# """
-# Function to compute a two-point function <psi|O(t)O(0)|psi>
-# at a list of times {t1,t2,...}. Since we're doing the same
-# operator at each time, we can optimize this a bit.
-# """
-# function timeseries(
-# 	psi :: Array{Complex{Float64}},
-# 	evolve :: Evolver,
-# 	times :: Array{Float64},
-# 	Op1 :: Array{Complex{Float64},2},
-# 	Op2:: Array{Complex{Float64},2}
-# )
-# 	len = length(psi)
-# 	# <psi| Op_1(t) Op_2(0)|psi>
-# 	# <psi| U^d D_m U Op_1 U^d D_p U Op_2|psi>
-# 	# (<psi| U^d) * D_m * (U Op_1 U^d) * D_p * (U Op_2 |psi>)
+"""
+Function to compute a two-point function <psi|O(t)O(0)|psi>
+at a list of times {t1,t2,...}. Since we're doing the same
+operator at each time, we can optimize this a bit.
+"""
+function timeseries(
+	psi :: Array{Complex{Float64}},
+	evolve :: Evolver,
+	times :: Array{Float64},
+	Op1 :: Array{Complex{Float64},2},
+	Op2:: Array{Complex{Float64},2}
+)
+	len = length(psi)
+	# <psi| Op_1(t) Op_2(0)|psi>
+	# <psi| U D_m U^d Op_1 U D_p U^d Op_2|psi>
+	# (<psi| U) * D_m * (U^d Op_1 U) * D_p * (U^d Op_2 |psi>)
 
-# 	UOp1Ud = BLAS.gemm('N','C',Op1,evolve.O)
-# 	UOp1Ud = BLAS.gemm('N','N',evolve.O,UOp1Ud)
+	UdOp1U = BLAS.gemm('N','C',Op1,evolve.O)
+	UdOp1U = BLAS.gemm('N','N',evolve.O,UdOp1U)
 
-# 	UOp2psi =  BLAS.gemv('N',Op2,psi) 
-# 	UOp2psi =  BLAS.gemv('N',evolve.O,UOp2psi) 
+	UdOp2psi =  BLAS.gemv('N',Op2,psi) 
+	UdOp2psi =  BLAS.gemv('N',evolve.O,UdOp2psi) 
 
-# 	Upsi = BLAS.gemv('N',evolve.O,psi)
+	Udpsi = BLAS.gemv('C',evolve.O,psi)
 
-# 	corr_t = Array{Complex{Float64}}(uninitialized, length(times))
-# 	for k in 1:length(times)
-# 		t = times[k]
-# 		expEigenvals_m = [exp(-im * t * ev) for ev in evolve.evs]
-# 		expEigenvals_p = [exp(im * t * ev) for ev in evolve.evs]
+	corr_t = Array{Complex{Float64}}(uninitialized, length(times))
+	for k in 1:length(times)
+		t = times[k]
+		expEigenvals_m = [exp(-im * t * ev) for ev in evolve.evs]
+		expEigenvals_p = [exp(im * t * ev) for ev in evolve.evs]
 
-# 		D_m = Diagonal(expEigenvals_m)
-# 		D_p = Diagonal(expEigenvals_p)
+		D_m = Diagonal(expEigenvals_m)
+		D_p = Diagonal(expEigenvals_p)
 
-# 		v = D_p * UOp2psi
-# 		v = BLAS.gemv('N',UOp1Ud,v)
-# 		v = D_m * v
+		v = D_p * UdOp2psi
+		v = BLAS.gemv('N',UdOp1U,v)
+		v = D_m * v
 
-# 		corr_t[k] = BLAS.dotc(len,Upsi,1,v,1)
-# 	end
+		corr_t[k] = BLAS.dotc(len,Udpsi,1,v,1)
+	end
 
-# 	return corr_t
-# end
+	return corr_t
+end
 
 """
 Function to compute a two-point function Tr[rho O(t)O(0)]
