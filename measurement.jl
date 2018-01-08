d7acc7be81df5d687227d5921e9c11013fd55de1
Daniel E. Parker <daniel_parker@berkeley.edu>
2018-01-08 10:13:14 -0800

    Added a function for R-staistics in measurements.jl. Verified against old mathematica code.

--- measurement.jl
+++ measurement.jl
@@ -159,3 +159,24 @@ function transition_finder(
 	)
 
 end
+
+
+"""
+Computes the R-statistic for a spectrum. Must be in a single symmetry sector. For integrable systems, R ~ 0.386. For GOE level-statistics, R ~0.5295. 
+"""
+#verified against mathematica code
+function r_statistic(
+	eigenvalues :: Array{Float64}
+	)
+	rs = Array{Float64}(uninitialized, length(eigenvalues)-2)
+
+	for i in 1:length(rs)
+		delta_na = eigenvalues[i+1]-eigenvalues[i]
+		delta_nb = eigenvalues[i+2]-eigenvalues[i+1]
+		r_min = min(delta_nb,delta_na)
+		r_max = max(delta_nb,delta_na)
+		r = (abs(r_min - r_max) < 10e-14) ? 1.0 : r_min/r_max
+		rs[i] = r
+	end
+	return mean(rs)
+end
